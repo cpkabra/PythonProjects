efec658ee470e4ea7a7d560feea8ef05f0d944df
Neel Patel <nvpatel4@ncsu.edu>
2020-04-20 02:07:43 -0400

    Implemented rock, paper, scissors game

--- PasswordGenerator/PasswordGenerator.py
+++ PasswordGenerator/PasswordGenerator.py
@@ -2,9 +2,8 @@ import random
 import string
 
 size = int(input("Please enter the length for the password: "))
-
 password_chars = string.ascii_letters + string.digits + string.punctuation
 generated_pass = ""
-for i in range(0, size):
+for i in range(size):
     generated_pass += random.choice(password_chars)
 print("Random password : " + generated_pass)
