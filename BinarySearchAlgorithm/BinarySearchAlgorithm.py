5e40f13ab35e3370b118f101626a481bf9f421be
Neel Patel <nvpatel4@ncsu.edu>
2020-04-21 20:03:49 -0400

    Implemented BinarySearchAlgorithm.py

---
+++ BinarySearchAlgorithm/BinarySearchAlgorithm.py
@@ -0,0 +1,19 @@
+
+def binary_search_algorithm(nums, num):
+    if len(nums) == 0:
+        return "Number not in the list"
+    mid_value = nums[int(len(nums)/2)]
+    if mid_value == num:
+        return "Number found!"
+    elif len(nums) == 1 and mid_value != num:
+        return "Number not in the list after"
+    elif num < mid_value:
+        left = nums[0:int(len(nums) / 2)]
+        return binary_search_algorithm(left, num)
+    elif num > mid_value:
+        right = nums[int(len(nums)/2) + 1: len(nums)]
+        return binary_search_algorithm(right, num)
+
+data = [number for number in range(0, 101) if number % 2 == 0]
+num_to_search = int(input("Please enter the number to search : "))
+print(str(num_to_search) + " : " + binary_search_algorithm(data, num_to_search))
