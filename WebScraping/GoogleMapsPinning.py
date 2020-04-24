40c579483bd01e6c428d32ddee656af152cc8ac0
Neel Patel <nvpatel4@ncsu.edu>
2020-04-24 01:56:52 -0400

    GoogleMapsPinning.py Take either a command line argument or a data from the system clipboard and directs user to that address on google maps using web browser library

---
+++ WebScraping/GoogleMapsPinning.py
@@ -0,0 +1,13 @@
+
+#@author Neel Patel
+#@file GoogleMapsPinning.py
+
+import webbrowser as wb
+import sys, pyperclip
+
+maps_address = "https://www.google.com/maps/place/"
+if len(sys.argv) > 1:
+    maps_address.join(sys.argv[1:])
+else:
+    maps_address += pyperclip.paste()
+wb.open(maps_address)
